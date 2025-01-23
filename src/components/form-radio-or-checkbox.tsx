804c57ae26a02a0ade7485ef790786505acfea59
yamoo9 <yamoo9@naver.com>
2025-01-23 15:50:35 +0900

    [실습] 파생 상태 활용

--- src/components/form-radio-or-checkbox.tsx
+++ src/components/form-radio-or-checkbox.tsx
@@ -4,15 +4,19 @@ type FormRadioOrCheckboxProps = ComponentProps<'input'> & {
   label: string;
 };
 
-function FormRadioOrCheckbox({ label, type, ...restProps }: FormRadioOrCheckboxProps) {
+function FormRadioOrCheckbox({
+  label,
+  type,
+  ...restProps
+}: FormRadioOrCheckboxProps) {
   const inputId = useId();
 
-  switch(type) {
+  switch (type) {
     case 'radio':
     case 'checkbox':
       return (
         <div className="formControl">
-          <input type={type} id={inputId} {...restProps} />
+          <input type={type} id={inputId} {...restProps} />{' '}
           <label htmlFor={inputId}>{label}</label>
         </div>
       );
