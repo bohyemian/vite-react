662aa0b21e8ece94e77f248e28baa2b74ce667e0
yamoo9 <yamoo9@naver.com>
2025-02-04 14:49:45 +0900

    [실습] 틱택토 게임 구현 part 1-1.
    - 컴포넌트 구조 설계
    - 컴포넌트 마크업
    - 레이아웃 스타일링

---
+++ src/homework/pages/tic-tac-toe/components/cell.tsx
@@ -0,0 +1,13 @@
+import { tm } from "@/utils/tw-merge";
+
+function Cell() {
+  return (
+    <button type="button" className={
+      tm('size-12 border rounded-md')
+    }>
+      셀
+    </button>
+  )
+}
+
+export default Cell;
