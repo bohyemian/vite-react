938c8ca0cdc6b4cbf0aac78d192fe0abd0c6c141
yamoo9 <yamoo9@naver.com>
2025-02-04 14:16:42 +0900

    [실습] 틱택토 게임 구현 part 1.
    - 컴포넌트 구조 설계
    - 컴포넌트 마크업

--- src/playground.tsx
+++ src/playground.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import Nav from '@/homework/components/nav';
-import { getUIView, type UIView } from '@/homework/lib/ui-view';
+import { getUIView } from '@/homework/lib/ui-view';
 import StateManagement from '@/homework/pages/state-management';
 import SignInForm from '@/homework/pages/sign-in';
 import SignUpForm from '@/homework/pages/sign-up';
+import TicTacToe from './homework/pages/tic-tac-toe';
 
-const getViewElement = (uiView: UIView) => {
+const getViewElement = (uiView: string) => {
   let viewElement: React.ReactElement | null = null;
-  
+
   switch (uiView) {
     case 'signin': {
       viewElement = <SignInForm />;
@@ -21,13 +22,17 @@ const getViewElement = (uiView: UIView) => {
       viewElement = <StateManagement />;
       break;
     }
+    case 'tic-tac-toe': {
+      viewElement = <TicTacToe />;
+      break;
+    }
   }
 
   return viewElement;
-}
+};
 
 function Playground() {
-  const [uiView] = useState<UIView>(getUIView);
+  const [uiView] = useState<string>(getUIView);
   const viewElement = getViewElement(uiView);
 
   return (
