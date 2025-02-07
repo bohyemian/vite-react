3567b2efe8e1f25610333c5531bb7eec815cc23d
yamoo9 <yamoo9@naver.com>
2025-02-07 14:53:25 +0900

    [실습] 오류 경계 5
    - react-error-boundary 오픈 소스 라이브러리 활용

--- src/pages/error-boundary/counter.tsx
+++ src/pages/error-boundary/counter.tsx
@@ -1,5 +1,5 @@
-import { tm } from "@/utils/tw-merge";
-import { useState } from "react";
+import { tm } from '@/utils/tw-merge';
+import { useState } from 'react';
 
 interface CounterProps {
   count?: number;
@@ -8,7 +8,12 @@ interface CounterProps {
   max?: number;
 }
 
-function Counter({ count: initialCount = 0, step = 1, min = 0, max = 10 }: CounterProps) {
+function Counter({
+  count: initialCount = 0,
+  step = 1,
+  min = 0,
+  max = 10,
+}: CounterProps) {
   // [상태: derived state from props]
   const [count, setCount] = useState(initialCount);
 
@@ -26,18 +31,32 @@ function Counter({ count: initialCount = 0, step = 1, min = 0, max = 10 }: Count
     const nextCount = count + step;
     if (nextCount > max) return;
     setCount(nextCount);
-  }
+  };
 
   // [렌더]
   return (
     <div className={tm('flex flex-col gap-2 items-start')}>
-      <output className={tm('font-semibold text-3xl text-react')}>{count} {doubleCounts}</output>
+      <output className={tm('font-semibold text-3xl text-react')}>
+        {count} {doubleCount}
+      </output>
       <div className={tm('flex', '*:hover:bg-sky-800 *:cursor-pointer')}>
-        <button type="button" className={tm('px-6 py-1 bg-react text-white rounded-l-full')} onClick={handleDecrease}>-{step}</button>
-        <button type="button" className={tm('px-6 py-1 bg-react text-white rounded-r-full')} onClick={handleIncrease}>+{step}</button>
+        <button
+          type="button"
+          className={tm('px-6 py-1 bg-react text-white rounded-l-full')}
+          onClick={handleDecrease}
+        >
+          -{step}
+        </button>
+        <button
+          type="button"
+          className={tm('px-6 py-1 bg-react text-white rounded-r-full')}
+          onClick={handleIncrease}
+        >
+          +{step}
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
 export default Counter;
