83c30e37481ffa220a86929ccf6607fa7c34e1e6
yamoo9 <yamoo9@naver.com>
2025-02-07 13:38:56 +0900

    [실습] 클래스 vs. 함수 컴포넌트
    - 상태
    - 파생된 상태
    - 상태 업데이트 (이벤트 핸들러)

--- src/pages/error-boundary/index.tsx
+++ src/pages/error-boundary/index.tsx
@@ -1,4 +1,5 @@
 import { tm } from '@/utils/tw-merge';
+import Counter from './counter';
 
 function ErrorBoundaryDemo() {
   return (
@@ -8,6 +9,8 @@ function ErrorBoundaryDemo() {
         오류 경계는 함수 컴포넌트로 구현 불가능합니다.<br />
         오직 클래스 컴포넌트로만 구현 가능합니다.
       </p>
+
+      <Counter step={2} min={4} max={12} count={6} />
     </section>
   );
 }
