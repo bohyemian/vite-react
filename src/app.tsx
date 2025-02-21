ed3c2ae444aecb6663936ba63715bf0046d7e460
yamoo9 <yamoo9@naver.com>
2025-02-21 14:31:50 +0900

    [실습] 인증 레이아웃 구성
    - 로그인 / 가입 페이지 공통 레이아웃
    - 아웃렛 컴포넌트 (배출구)
    - 중첩된 레이아웃

--- src/app.tsx
+++ src/app.tsx
@@ -6,9 +6,9 @@ import SignInPage from './pages/sign-in/page';
 import SignUpPage from './pages/sign-up/page';
 import PrintError from './components/error';
 import HomePage from './pages/home/page';
+import AuthLayout from './layouts/auth/layout';
 
 function App() {
-  
   return (
     <ErrorBoundary FallbackComponent={PrintError}>
       <ThemeProvider>
@@ -18,8 +18,13 @@ function App() {
             <Route index element={<HomePage />} />
             <Route path="/playground" element={<PlaygroundPage />} />
 
-            <Route path="/signin" element={<SignInPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
+            {/* 레이아웃 */}
+            <Route path="/auth" element={<AuthLayout />}>
+              {/* 중첩된 라우트(Nested Routes) */}
+              {/* 페이지는 배출구(Outlet)에서 렌더링 */}
+              <Route path="signin" element={<SignInPage />} />
+              <Route path="signup" element={<SignUpPage />} />
+            </Route>
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
